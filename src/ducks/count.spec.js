179cdf84615380d293a3529d18cbb8fbef1ff5f1
Matt Granmoe <mattgranmoe@gmail.com>
2017-09-23 15:18:37 -0500

    Tests working, simplified babelrc for test env

--- src/ducks/count.spec.js
+++ src/ducks/count.spec.js
@@ -4,9 +4,18 @@ import { incrementAsync } from 'ducks/count'
 
 const store = initStore()
 
+const initialState = {
+  count: 0,
+  router: {
+    pathname: 'blank',
+    query: {},
+    search: ''
+  }
+}
+
 describe('incrementAsync', () => {
   it('dispatches the increment action after one second', async () => {
     await store.dispatch(incrementAsync())
-    expect(store.getState()).toEqual({ count: 1 })
+    expect(store.getState()).toEqual({ ...initialState, count: 1 })
   })
 })
